3e8b88133e6bc2620e007bde75e5957cfcf0657a
Aya Afaghani <aya.afagany@student.uni-tuebingen.de>
2023-05-23 14:49:44 +0200

    Changed blade of profile

--- Tue_Tweet/resources/views/profile.blade.php
+++ Tue_Tweet/resources/views/profile.blade.php
@@ -32,11 +32,6 @@
 									<label class="form-label">Last Name *</label>
 									<input type="text" class="form-control" placeholder="" aria-label="Last name" value="Doe">
 								</div>
-								<!-- Phone number -->
-								<div class="col-md-6">
-									<label class="form-label">Phone number *</label>
-									<input type="text" class="form-control" placeholder="" aria-label="Phone number" value="(333) 000 555">
-								</div>
 								<!-- Mobile number -->
 								<div class="col-md-6">
 									<label class="form-label">Mobile number *</label>
@@ -47,11 +42,7 @@
 									<label for="inputEmail4" class="form-label">Email *</label>
 									<input type="email" class="form-control" id="inputEmail4" value="example@homerealty.com">
 								</div>
-								<!-- Skype -->
-								<div class="col-md-6">
-									<label class="form-label">Skype *</label>
-									<input type="text" class="form-control" placeholder="" aria-label="Phone number" value="Scaralet D">
-								</div>
+
 							</div> <!-- Row END -->
 						</div>
 					</div>
@@ -88,31 +79,12 @@
 									<label class="form-label"><i class="fab fa-fw fa-facebook me-2 text-facebook"></i>Facebook *</label>
 									<input type="text" class="form-control" placeholder="" aria-label="Facebook" value="http://www.facebook.com">
 								</div>
-								<!-- Twitter -->
-								<div class="col-md-6">
-									<label class="form-label"><i class="fab fa-fw fa-twitter text-twitter me-2"></i>Twitter *</label>
-									<input type="text" class="form-control" placeholder="" aria-label="Twitter" value="http://www.twitter.com">
-								</div>
-								<!-- Linkedin -->
-								<div class="col-md-6">
-									<label class="form-label"><i class="fab fa-fw fa-linkedin-in text-linkedin me-2"></i>Linkedin *</label>
-									<input type="text" class="form-control" placeholder="" aria-label="Linkedin" value="http://www.linkedin.com">
-								</div>
 								<!-- Instragram -->
 								<div class="col-md-6">
 									<label class="form-label"><i class="fab fa-fw fa-instagram text-instagram me-2"></i>Instagram *</label>
 									<input type="text" class="form-control" placeholder="" aria-label="Instragram" value="http://www.instragram.com">
 								</div>
-								<!-- Dribble -->
-								<div class="col-md-6">
-									<label class="form-label"><i class="fas fa-fw fa-basketball-ball text-dribbble me-2"></i>Dribble *</label>
-									<input type="text" class="form-control" placeholder="" aria-label="Dribble" value="http://www.dribble.com">
-								</div>
-								<!-- Pinterest -->
-								<div class="col-md-6">
-									<label class="form-label"><i class="fab fa-fw fa-pinterest text-pinterest"></i>Pinterest *</label>
-									<input type="text" class="form-control" placeholder="" aria-label="Pinterest" value="http://www.pinterest.com">
-								</div>
+								
 							</div> <!-- Row END -->
 						</div>
 					</div>
