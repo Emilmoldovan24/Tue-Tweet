642604e4c396940580d62f0203bd29e4bfddaaa6
lukas3540 <lukas.lorentz@student.uni-tuebingen.de>
2023-06-08 16:46:17 +0200

    hideTweet now checks if tweet is already hidden, hides if visibile, unhides if not

--- Tue_Tweet/app/Http/Controllers/AdminController.php
+++ Tue_Tweet/app/Http/Controllers/AdminController.php
@@ -79,7 +79,16 @@ class AdminController extends Controller
     public function hideTweet(Request $request){
         
         $id = $request->id;
-        DB::update("update tweets set visibility = 0 where tweet_id = '$id'");
+        
+        $tweet = DB::select("select * from tweets where tweet_id ='$id'");
+        $tweetVis = DB::table('tweets')->where('tweet_id', $id)->value('visibility');
+
+        if($tweetVis == 0){
+            DB::update("update tweets set visibility = 1 where tweet_id = '$id'");
+        }else{
+            DB::update("update tweets set visibility = 0 where tweet_id = '$id'");
+        }
+        
         
         return redirect()->route('adminFeed');
     }
