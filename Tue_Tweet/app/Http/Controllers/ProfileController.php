827f518cca1ac0f5a50f24a5f800747a91ecdc37
LukeVallon <lukevallon02@gmail.com>
2023-06-08 22:32:04 +0200

    created links from names to profile page

--- Tue_Tweet/app/Http/Controllers/ProfileController.php
+++ Tue_Tweet/app/Http/Controllers/ProfileController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function getProfile() {
-        return view('profile');
+    public function getProfile($username) {
+        $profileUsername = $username;
+        /*$profile = User::where('username', $this->profileUsername)->first();
+        $profileID = $profile->id;
+        $profilePic = $profile->profile_img;
+        $profileBio = $profile->profile_bio;*/
+
+        return view('profile',compact('profileUsername'));
     }
 }
