bd5b8f629ed0186bc85aec02e1687d2889d264ce
Aya Afaghani <aya.afagany@student.uni-tuebingen.de>
2023-06-09 12:51:34 +0200

    New push

--- Tue_Tweet/resources/views/settings.blade.php
+++ Tue_Tweet/resources/views/settings.blade.php
@@ -117,73 +117,7 @@ My Profile settings
 	</div>
 
 	
-				{{-- <!-- Form START -->
-				<form class="file-upload">
-					<div class="row mb-5 gx-5">
-						<!-- Contact detail -->
-						<div class="col-xxl-8 mb-5 mb-xxl-0">
-							<div class="bg-secondary-soft px-4 py-5 rounded">
-								<div class="row g-3">
-									<h4 class="mb-4 mt-0">Contact detail</h4>
-									<!-- First Name -->
-									<div class="col-md-6">
-										<?php
-										$id = Auth::id();
-										$results = DB::table('users')->where('id', $id)->value('username');
-										echo '<label class="form-label">First Name *</label>';
-										echo '<input type="text" class="form-control" placeholder="" aria-label="First name" value=' . $results . '>';
-										?>
-									</div>
-									<!-- Email -->
-									<div class="col-md-6">
-										<?php
-										$id = Auth::id();
-										$results = DB::table('users')->where('id', $id)->value('email');
-										echo '<label for="inputEmail4" class="form-label">Email *</label>';
-										echo '<input type="email" class="form-control" id="inputEmail4" value=' . $results . '>';
-										?>
-									</div>
-									<!-- Bio -->
-									<div class="col-md-6">
-										<?php
-										$id = Auth::id();
-										$results = DB::table('users')->where('id', $id)->value('profile_bio');
-										echo '<label class="form-label">Bio *</label>';
-										echo '<input type="text" class="form-control" placeholder="" aria-label="Bio" value=' . $results . '>';
-										?>
-									</div>
-
-								</div> <!-- Row END -->
-							</div>
-						</div>
-						<!-- Upload profile -->
-
-					</div> <!-- Row END -->
-
-
-					<!-- change password -->
-					<div class="col-xxl-6">
-						<div class="bg-secondary-soft px-4 py-5 rounded">
-							<div class="row g-3">
-								<h4 class="my-4">Change Password</h4>
-								<!-- Old password -->
-								<div class="col-md-6">
-									<label for="exampleInputPassword1" class="form-label">Old password *</label>
-									<input type="password" class="form-control" id="exampleInputPassword1">
-								</div>
-								<!-- New password -->
-								<div class="col-md-6">
-									<label for="exampleInputPassword2" class="form-label">New password *</label>
-									<input type="password" class="form-control" id="exampleInputPassword2">
-								</div>
-								<!-- Confirm password -->
-								<div class="col-md-12">
-									<label for="exampleInputPassword3" class="form-label">Confirm Password *</label>
-									<input type="password" class="form-control" id="exampleInputPassword3">
-								</div>
-							</div>
-						</div>
-					</div> --}}
+				
 			</div> <!-- Row END -->
 			<!-- button -->
 			<div class="gap-3 d-md-flex justify-content-md-end text-center">
