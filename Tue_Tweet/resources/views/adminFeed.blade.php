1add9d27ff1d4c2f4be3cfe2590436332a6f6365
bennowagus <bennowagus@web.de>
2023-07-12 03:28:26 +0200

    safety

--- Tue_Tweet/resources/views/adminFeed.blade.php
+++ Tue_Tweet/resources/views/adminFeed.blade.php
@@ -15,7 +15,8 @@
 
 <style>
 div{
-    max-width: 500px
+    
+    max-width: 600px
 }
 body {
     background-color:  #E7E7E7;
@@ -85,7 +86,23 @@ margin-right: 10px;
   
   }
 }
+/*#comments{
+    padding = 50px 100px;
+}
+*/
 </style>
+
+
+<script>
+    function showComments(tweetID){
+        var x = document.getElementById("comments");
+        if(x.style.display == "none"){
+            x.style.display = "block";
+        }else{
+            x.style.display = "none";
+        }
+    }
+</script>
 
 <body>
 <nav class="navbar">
@@ -115,32 +132,6 @@ margin-right: 10px;
         </div>
 
         <div class="main-content">
-            <script>
-            // toggles the display of the comments when the user clicks on the comments button
-            function displayComments(tweet_id, tweet_typ) {
-
-
-                let element = document.getElementById("comments_" + tweet_typ + "_" + tweet_id);
-                element.removeAttribute("hidden");
-
-                if (element.style.display == "none" || element.style.display == "") {
-                    // show
-                    element.style.display = "block";
-                } else {
-                    // hide
-                    element.style.display = "none";
-                }
-            }
-
-
-            $(document).ready(function() {
-                $(".default_picture").on("error", function() {
-                    $(this).attr('src',
-                        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-                    );
-                });
-            });
-            </script>
 
             <div class="menu">
                 <a href="adminFeed?"> Visible Tweets </a> |
@@ -270,7 +261,6 @@ margin-right: 10px;
                             $likes = DB::table('likes')->where('tweet_id', $tweet->tweet_id)->count();
                             $numComments = DB::table('comments')->where('tweet_id', $tweet->tweet_id)->count();
                             $retweets = DB::table('retweets')->where('tweet_id', $tweet->tweet_id)->count();
-                            $tweet_typ =  (string) $tweet->typ;
                             $tweetImg = DB::table('tweets')
                             ->where('tweet_id', $tweet->tweet_id)
                             ->value('img');
@@ -305,87 +295,45 @@ margin-right: 10px;
                                 ?>
                                 
                                 <br>
-
-                                <!-- Comment-->
-                <?php echo '<div class="comments" id="comments_' . $tweet_typ . '_' . $tweet->id . '" hidden>'; ?>
-                <br>
-                <div class="comment-container">
-
-                    <!-- List Comments -->
-                    <?php
-                                // Get all comments for this tweet or retweet
-                        if($tweet->typ == 'tweet'){
-                            $comments = DB::table('comments')->where('tweet_id', $tweet->id)->where('deleted_at', NULL)->where('visibility', 1)->get();
-                        }elseif($tweet->typ == 'retweet'){
-                            $comments = DB::table('comments')->where('retweet_id', $tweet->id)->where('deleted_at', NULL)->where('visibility', 1)->get();
-                        }
-                        foreach ($comments as $comment) {
-                            $commentUsername = DB::table('users')->where('id', $comment->user_id)->value('username');
-                            $userImg = DB::table('users')->where('id', $user_id)->value('profile_img');
-                            $commentText = $comment->comment;
-                        ?>
-                    <div class="comment-post-container">
-                        <div class="post-row">
-                            <div class="user-profile">
-                                <?php echo $userImgHtml; ?>
-                                <div style="display: inline-block;">
-                                    <a style="margin-right: 3px; text-decoration: none;"
-                                        href="/{{ $commentUsername }}">{{ $commentUsername }}</a>
-                                    <a> &bull; </a>
-                                    <span>{{ $comment->created_at }}</span>
-                                </div>
-                            </div>
-
-
-                            @if ($comment->user_id === $cur_user_id)
-                            <div class="menu-btn-own">
-                                <button class="btn btn-dark" type="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-                                <ul class="dropdown-menu">
-                                    <li><button class="dropdown-item"><a
-                                                href="{{ route('MyCommentDelete', $comment->comment_id) }}"
-                                                style="text-decoration: none;">Delete</a></button>
-                                    </li>
-                                    <?php
-                                                    echo '<li><button type="button" class="dropdown-item" onclick="editComment(' . $comment->comment_id . ', ' . htmlspecialchars('"' . $comment->comment . '"') . ')" data-comment-id="' . $comment->comment_id . '" data-bs-toggle="modal" data-bs-target="#EditCommentModal">Edit</button></li>';
-                                                    ?>
-                                </ul>
-                            </div>
-                            @endif
-
-                        </div>
-                        <p>{{ $comment->comment }}</p>
-                    </div>
-                    <?php } ?>
-
-                    <!-- Comment input field -->
-                    <div class="comment-input">
-                        <form action="{{ route('postComment') }}" method="POST">
-                            @csrf
-                            <div class="input-group mb-3">
-                                <input type="text" name="comment" id="comment" class="form-control"
-                                    placeholder="Add a comment" aria-label="Add a comment"
-                                    aria-describedby="button-addon2">
-                                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Post</button>
-                            </div>
-                            <input type="hidden" name="tweet_typ" value="{{ $tweet->typ }}">
-                            <input type="hidden" name="tweet_id" value="{{ $tweet->id }}">
-                            <input type="hidden" name="user_id" value="{{ $user_id }}">
-                        </form>
-                    </div>
-                </div>
+                                <input type="button" name="comments" value="Show Comments" onclick="showComments($tweetID)" />
+                                <button onlclick="showComments"($tweetID)> Show Comments </button>
                                 <button class="delete-btn"><a href="{{ route('tweet.hide', $tweet->tweet_id) }}">Hide Tweet</a></button>
                                 <button class="delete-btn"><a href="{{ route('tweet.delete', $tweet->tweet_id) }}">Delete Tweet</a></button>
-
-                        </div>
-                                <?php
                                 
-                        }
+
+                                <?php
+                                if($numComments > 0){
+                                    
+                                    $comments = $comments = DB::table('comments')->where('user_id', $tweet->user_id)->get();
+                                    if($numComments > 0){
+                                        $comments = $comments = DB::table('comments')->where('user_id', $tweet->user_id)->get();
+                                        foreach($comments as $comment){
+                                            if($comment->user_id == $tweet->user_id){
+                                                ?>
+                                                <div id="comments" style="display:none;" class="comment_list">
+                                                    <?php
+                                                        echo "$comment->comment".
+                                                        '<br>';
+                                                        echo "$comment->comment_id".
+                                                        '</br>'
+                                                    ?>
+                                                </div>
+                                                <?php
+                                            }
+                                        }
+                                    }
+                                }
+                                ?>
+                                </div>
+                                <?php 
+                                
+                                
+                            }
+                    }       
                     
-                    }
             
         }
-                
+        
             
             ?>
         </div>
