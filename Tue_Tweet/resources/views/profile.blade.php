b2d1ea2e989e10d2d9ff2b8cd068e5f79fe67600
LukeVallon <lukevallon02@gmail.com>
2023-07-16 13:23:37 +0200

    Like Button works on profile now

--- Tue_Tweet/resources/views/profile.blade.php
+++ Tue_Tweet/resources/views/profile.blade.php
@@ -745,6 +745,7 @@ position: sticky;
                                                 <input type="hidden" name="retweet_id" value="{{ $tweet->id }}">
                                             @endif
                                             <input type="hidden" name="typ" value="{{ $tweet->typ }}">
+                                            <input type="hidden" name="user_id" value="{{ $user_id }}">
                                         </div>
                                     </form>
                                 </div>
